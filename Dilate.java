33f0874b0f69d0d5eb4d98c821bffe69aae871cb
Francesco Marinelli <francesco.marinelli@outlook.com>
2022-02-10 23:35:17 +0100

    import errors solved

--- Dilate.java
+++ Dilate.java
@@ -1,21 +1,28 @@
 import java.awt.image.BufferedImage;
-import java.io;
-import javax.imageio;
+import java.io.*;
+import javax.imageio.*;
 
 public class Dilate {
   public static void main(String args[]) {
     if(args[0] == null)
     {
-      System.out.println("Usage: java dilate <image filename>");
+      System.err.println("Usage: java dilate <image path>");
       System.exit(1);
     }
-    BufferedImage img=ImageIO.read(new File(args[0]));
+    BufferedImage img = null;
+    try {
+      img = ImageIO.read(new File(args[0]));
+    } catch (IOException e){
+      System.err.println("The path to the file must be wrong: reading failed");
+      System.exit(2);
+    }
     int width = img.getWidth(null);
     int height = img.getHeight(null);
     int[][] src = new int[width][height];
-    for (int i = 0; i < width; i++)
-      for (int j = 0; j < height; j++)
+    for (int i = 0; i < width; i++) {
+      for (int j = 0; j < height; j++) {
         src[i][j] = img.getRGB(i, j);
-
+      }
+    }
   }
 }
